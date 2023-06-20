70ee3c01b6d8b24ea0caf6ab9ad211f677ba84e6
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-06-20 10:36:38 -0500

    corrigo la posicion del app menu items

--- lib/presentation/screens/home/home_sceen.dart
+++ lib/presentation/screens/home/home_sceen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,18 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {},
     );
   }
 }
