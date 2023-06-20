fc7467661f75fc31a83b36da149c63220f4998a6
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-06-20 09:56:06 -0500

    modificamos estilos de appbar

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -20,7 +20,7 @@ class AppTheme {
             'Selected color must be less or equal than ${colorList.length - 1}');
 
   ThemeData getTheme() => ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: colorList[selectedColor],
-      );
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: const AppBarTheme(centerTitle: false));
 }
