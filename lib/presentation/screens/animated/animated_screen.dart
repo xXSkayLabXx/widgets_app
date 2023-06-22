77d55399a8a4fbd510f0118fdb25f09776326e4c
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-06-22 13:07:57 -0500

    screen creados y barril actualizada

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+  static const name = 'animate_screen';
+  const AnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScrren extends StatelessWidget {
+  static const name = 'tutorial_screen';
+  const AppTutorialScrren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
