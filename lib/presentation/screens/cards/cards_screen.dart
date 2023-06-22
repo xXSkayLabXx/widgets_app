f104f23b8a816a110397dc74961014703dc6a033
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-06-22 09:49:29 -0500

    rutas con nombre implementado

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({super.key});
+  static const String name = 'cards_screen';
 
   @override
   Widget build(BuildContext context) {
--- lib/presentation/screens/home/home_sceen.dart
+++ lib/presentation/screens/home/home_sceen.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  static const String name = 'home_screen';
 
   @override
   Widget build(BuildContext context) {
@@ -57,6 +60,7 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
+        ///context.pushNamed(CardScreen.name);
         context.push(menuItem.link);
         //Navigator.pushNamed(context, menuItem.link);
 
